50be25a0082681613ee8bbdb550efc3e65f4c38a
intsuc <intsuc@gmail.com>
2017-12-10 23:23:39 +0900

    test

---
+++ src/extension.ts
@@ -0,0 +1,60 @@
+'use strict';
+import * as vscode from 'vscode';
+import * as parser from './parser';
+import { languages, ExtensionContext, Range, SignatureInformation, CompletionItem, CompletionItemKind, commands } from 'vscode';
+import { configure } from 'vscode/lib/testrunner';
+
+export function activate(context: ExtensionContext) {
+
+    var completion = languages.registerCompletionItemProvider('mcfunction', {
+        provideCompletionItems(document, position, token) {
+            var text = document.getText(new Range(position.line, 0, position.line, position.character))
+
+            var results = parser.parse(text);
+            var args: string[] = results.args;
+            var type: string = results.type;
+
+            var items: CompletionItem[] = [];
+
+            args.forEach(arg => {
+                var item = new CompletionItem(arg);
+                item.filterText = arg;
+                switch (type) {
+                    case "command":
+                        item.kind = CompletionItemKind.Function;
+                        break;
+                    case "literal":
+                        item.kind = CompletionItemKind.Property;
+                        break;
+                    default:
+                        item.kind = CompletionItemKind.Enum;
+                        break;
+                }
+                items.push(item);
+            });
+
+            return items;
+        }
+    }, ...[' ', '[', '{', ',']);
+
+    var signature = languages.registerSignatureHelpProvider('mcfunction', {
+        provideSignatureHelp(document, position, token) {
+            var text = document.getText(new Range(position.line, 0, position.line, position.character));
+
+            var results = parser.parse(text);
+            var usage = results.usage;
+
+            if (usage != null) {
+                var help = new vscode.SignatureHelp();
+                help.signatures = [new SignatureInformation(usage)];
+                help.activeSignature = 0;
+                help.activeParameter = 0;
+
+                return help;
+            }
+        }
+    }, ...[' ']);
+}
+
+export function deactivate() {
+}
